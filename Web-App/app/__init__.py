c5d3b4de4c936f683b5001808160621afdd337bf
knangia04 <54154856+knangia04@users.noreply.github.com>
2022-01-31 14:46:41 -0600

    Email Error Notification Part 2

--- Web-App/app/__init__.py
+++ Web-App/app/__init__.py
@@ -1,5 +1,6 @@
 import logging
 from logging.handlers import RotatingFileHandler
+from logging.handlers import SMTPHandler
 import os
 
 from flask import Flask
@@ -42,3 +43,17 @@ if True:
         app.logger.setLevel(logging.DEBUG)
         app.logger.info('HalfCaf startup')
 
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='Half Caf App Error', credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
