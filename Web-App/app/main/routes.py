7baca6c9ff3e836787ea2410e19e5c9f9cdc9170
s <70646248+sopli2@users.noreply.github.com>
2022-02-14 15:09:12 -0600

    Template fix

--- Web-App/app/main/routes.py
+++ Web-App/app/main/routes.py
@@ -476,7 +476,7 @@ def a_userDashboard():
 @app.route('/reset_password_request', methods=['GET', 'POST'])
 def reset_password_request():
         if current_user.is_authenticated:
-                return redirect(url_for('index'))
+                return redirect(url_for('main.login'))  #main.index is what's on the tutorial
         form = ResetPasswordRequestForm()
         if form.validate_on_submit():
                 user = User.query.filter_by(email=form.email.data).first()
