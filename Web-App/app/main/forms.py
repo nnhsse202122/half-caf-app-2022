934bb6cc510a14b5d746dd88a552a3e31494d6a4
s <70646248+sopli2@users.noreply.github.com>
2022-02-18 14:26:32 -0600

    Register email

--- Web-App/app/main/forms.py
+++ Web-App/app/main/forms.py
@@ -156,6 +156,8 @@ class A_DeleteUserForm(FlaskForm):
 
 class ResetPasswordRequestForm(FlaskForm):
     email = StringField('Email', validators={DataRequired(), Email()})
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Request Password Reset')
 
 class ResetPasswordForm(FlaskForm):
