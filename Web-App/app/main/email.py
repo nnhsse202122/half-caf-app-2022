973566d3e91abc48c6ca49d8bee08529497fa5cd
Divya <divya.lidder1@gmail.com>
2022-02-26 19:38:12 -0600

    added base html formatting to emails

--- Web-App/app/main/email.py
+++ Web-App/app/main/email.py
@@ -4,9 +4,14 @@ from app import mail
 def order_email(name, order, subject, sender, recipients):
     msg = Message(subject, sender = sender, recipients = recipients)
     msg.body = "Hi " + name + "! Thanks for ordering from the Half-Caf! Your order has been completed and you will be recieving it shortly. Here is what you have ordered: " + str(order) 
+    msg.html = "Hi " + name + "! Thanks for ordering from the Half-Caf! <br> + \
+    Your order has been completed and you will be receiving it shortly. <br> Here is what you have ordered: " + <b> str(order)</b>
     mail.send(msg)
 
 def reg_email(user):
     msg = Message("Please activate new user", sender = 'nnhshalfcafapp@gmail.com', recipients = ['nnhshalfcafapp@gmail.com'])
     msg.body = "username: " + user.username + " user type: " + user.user_type + " user email: " + user.email_address
+    msg.html = "<b> Username: </b>" + user.username + "<br>" + "<b> User Type: </b>" + user.user_type + "<br>" + \
+    +   "<b> User Email Address: </b>" + user.email_address + \
+    +    "<a href="https://nnhshalfcaf.com">Half-Caf App</a>" 
     mail.send(msg)
