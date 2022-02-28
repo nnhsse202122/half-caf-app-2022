2b9a94aa2a0dcfd8768ba480abb4c1b6d90f3a4f
dslidder <56369997+dslidder@users.noreply.github.com>
2022-02-28 15:11:28 -0600

    formatted email and added username icon

--- Web-App/app/main/email.py
+++ Web-App/app/main/email.py
@@ -4,14 +4,13 @@ from app import mail
 def order_email(name, order, subject, sender, recipients):
     msg = Message(subject, sender = sender, recipients = recipients)
     msg.body = "Hi " + name + "! Thanks for ordering from the Half-Caf! Your order has been completed and you will be recieving it shortly. Here is what you have ordered: " + str(order) 
-    msg.html = "Hi " + name + "! Thanks for ordering from the Half-Caf! <br> + \
-    Your order has been completed and you will be receiving it shortly. <br> Here is what you have ordered: " + <b> str(order)</b>
+    msg.html = "Hi " + name + "! Thanks for ordering from the Half-Caf! <br> \
+    Your order has been completed and you will be receiving it shortly. <br> Here is what you have ordered: " + str(order)
     mail.send(msg)
 
 def reg_email(user):
     msg = Message("Please activate new user", sender = 'nnhshalfcafapp@gmail.com', recipients = ['nnhshalfcafapp@gmail.com'])
     msg.body = "username: " + user.username + " user type: " + user.user_type + " user email: " + user.email_address
-    msg.html = "<b> Username: </b>" + user.username + "<br>" + "<b> User Type: </b>" + user.user_type + "<br>" + \
-    +   "<b> User Email Address: </b>" + user.email_address + \
-    +    "<a href="https://nnhshalfcaf.com">Half-Caf App</a>" 
+    msg.html = "<b> Username: </b>" , user.username , "<br> <b> User Type: </b>" , user.user_type + "<br>" + \
+    +   "<b> User Email Address: </b>" , user.email_address
     mail.send(msg)
