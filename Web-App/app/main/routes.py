749b11c2d905d6d77530a0c975df7e38129a6ba1
NNHS203 <hliu@stu.naperville203.org>
2022-03-22 15:10:53 -0500

    error page

--- Web-App/app/main/routes.py
+++ Web-App/app/main/routes.py
@@ -561,3 +561,12 @@ def enable():
         store.acc_order = True
         db.session.commit()
         return redirect(url_for('main.barista'))
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+    
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
